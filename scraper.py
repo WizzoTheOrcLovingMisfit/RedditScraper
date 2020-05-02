7a445bf3f82d6811111cceaa967dde983c07ecde
Ethan Frater <ebfrater@yahoo.com>
2020-05-01 23:56:08 -0400

    fixed all regex and make loops for testing

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import re
 import requests
 from urllib.parse import urlparse
 import html2text
-import tkinter
+
 
 def cleanser(posts):
     bad_chars = ['(', ')']
@@ -15,16 +15,22 @@ def cleanser(posts):
 
 def get_posts(webpage):
        print('The following posts were found') 
-       posts=re.findall('\(\/r\/*.+\/\)', str(webpage))
+       posts=re.findall("old.reddit.com\/r\/[a-zA-Z0-9_/]+", str(webpage))
        posts="\n".join(posts)
-       cleanser(posts)
+      
        return posts
 
 def get_users(webpage):
     print('The following users were found') 
-    users=re.findall('reddit.com/user/[a-zA-Z0-9].+\)', str(webpage))
+    users=re.findall('reddit.com/user/[a-zA-Z0-9]+', str(webpage))
     users="\n".join(users)
     return users
+
+def get_subreddits(webpage): 
+    print("the following subewddits are referenced here")
+    subreddits=re.findall('/r/\w+', str(webpage))
+    subreddits="\n".join(subreddits)
+    return subreddits
 
 url = 'https://old.reddit.com/r/all/'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'}
@@ -32,19 +38,29 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gec
 get_request=html2text.html2text(requests.get(url,headers=headers).text)
 print("The page shows:\r\n")
 print(get_request)
+while 1:
+    option = input("Are you interested in users or posts or subs? ")
+    if 'user' in option:
 
-option = input("Are you interested in users or posts? ")
-if 'user' in option:
+        users=get_users(get_request)
+    
+    
+        print(users)
+    elif 'post' in option:
+        posts=get_posts(get_request)
+    
+    
+        print(posts)
 
-    users=get_users(get_request)
+    elif 'reddit' in option:
+        subreddits=get_subreddits(get_request)
     
     
-    print(users)
-elif 'post' in option:
-    posts=get_posts(get_request)
-    
-    
-    print(posts)
+        print(subreddits)
+    else:
+        break
+    option=""
+
 
 
 
